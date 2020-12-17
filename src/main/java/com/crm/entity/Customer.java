64bf020409593ed91a1919cfd1834c0f7cd39316
4178392 <susana.gamboa@globant.com>
2020-12-17 15:23:10 -0300

    first commit

---
+++ src/main/java/com/crm/entity/Customer.java
@@ -0,0 +1,20 @@
+package com.crm.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+}
