0a513f91372703b07c103ff9604deb25f5fc7815
4178392 <susana.gamboa@globant.com>
2020-12-17 18:22:42 -0300

    Customer list implementation

--- src/main/java/com/crm/controller/CustomerController.java
+++ src/main/java/com/crm/controller/CustomerController.java
@@ -1,15 +1,25 @@
 package com.crm.controller;
 
+import com.crm.dao.CustomerDAO;
+import com.crm.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model model){
-        return "list-customers.html";
+        List<Customer> customers = customerDAO.getCustomers();
+        model.addAttribute("customers" , customers);
+        return "list-customers";
     }
 }
--- src/main/java/com/crm/dao/CustomerDAOImpl.java
+++ src/main/java/com/crm/dao/CustomerDAOImpl.java
@@ -1,2 +1,26 @@
-package com.crm.dao;public class CustomerDAOImpl {
+package com.crm.dao;
+
+import com.crm.entity.Customer;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        Session session = entityManager.unwrap(Session.class);
+
+        List<Customer> customers = entityManager.createQuery("from Customer", Customer.class).getResultList();
+        return customers;
+    }
 }
